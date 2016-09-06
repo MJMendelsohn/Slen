a84fc39ea8b4f08a7eefb588f1ee8918b12dda91
Michael Mendelsohn <Michael@Michaels-MacBook-Pro.local>
2016-09-05 20:34:05 -0400

    Creates Simple Board

---
+++ src/Board.java
@@ -0,0 +1,20 @@
+public final class Board {
+  private int[][] board;
+  
+  public Board(int rows, int cols) {
+    this.board = new int[rows][cols];
+  }
+
+  public void setCell(int row, int col, int val) {
+    board[row][col] = val;
+  }
+
+  public void printBoard() {
+    for(int i = 0; i < board.length; i++) {
+      for(int j = 0; j < board[i].length; j++ {
+        System.out.print(board[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+}
