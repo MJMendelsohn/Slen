5485b00134b0ce4914fd3837af552b7e1ac5eaa5
Jacob Lettie <jacoblettie@gmail.com>
2016-09-16 13:28:39 -0400

    Added react.js to the project and moved React and JQuery to a libs folder for organization.

--- src/view.js
+++ src/view.js
@@ -10,39 +10,5 @@ function View(gameData) {
 }
 
 View.prototype.display = function() {
-    // function transform ( arr ) {
-    //     var result = [], temp = [];
-    //     arr.forEach( function ( elem, i ) {
-    //         if ( i > 0 && i % 5 === 0 ) {
-    //             result.push( temp );
-    //             temp = [];
-    //         }
-    //         temp.push( elem );
-    //     });
-    //     if ( temp.length > 0 ) {
-    //         result.push( temp );
-    //     }
-    //     return result;
-    // }
-    $.get('templates.html', function(template) {
-        $.tmpl(template, data).appendTo('#whatever');
-    });
 
-    $.get("templates.html", function(html) { $(this.el).html(Mustache.render(html, view)) });
-
-    data = this.gameData.board.xIndices;
-    console.log($("#gameBoard"));
-    $("body").append(Mustache.render($("#gameBoard").html(), data));
-
-    // var data = {
-    //     title: "Constructing HTML Elements"
-    // }
-    // var template = [
-    //     '<div class="tutorial">',
-    //         '<h1 class="tutorial-heading">{{title}}<h1>',
-    //     '</div>'
-    // ].join("\n");
-
-    // var html = Mustache.render(template, data);
-    // $("body").append(html);
 };
