33aefccd8895cdb089ac39e74c0c00ac6a891b8d
Jacob Lettie <jacoblettie@gmail.com>
2016-09-28 23:26:07 -0400

    Carried out initial port of capturing logic from Java. Currently not functional, but not crashing either.

--- src/model.js
+++ src/model.js
@@ -28,6 +28,7 @@ Model.prototype.update = function(command) {
             } else {
                 this.currentColor = State.BLACK;
             }
+            this.computeTerritory();
         }
     }
     this.viewCallback.apply(this, [this.gameData]);
@@ -40,4 +41,162 @@ Model.prototype.place = function(x, y, value) {
     }
     this.gameData.getBoardData().getLocationData(x, y).addPiece(value);
     return true;
+};
+
+Model.prototype.getCell = function(x, y) {
+    return this.gameData.getBoardData().getLocationData(x, y);
+};
+
+Model.prototype.computeTerritory = function() {
+    // capturing logic requires simultaneity, so we process column by column
+    var noMoreCaptures = true;
+    /**
+     * Maps for each color from a given interval to the column it appears in. Will only ever need one entry since if a
+     * second pair is found, scoring would occur which will reset the map entry with the new segment.
+     */
+    blackSegmentColumns = {};
+    whiteSegmentColumns = {};
+    /**
+     * Sets for each color containing the vertical segments of the currently examined column, where the pairs represent
+     * interval endpoints.
+     */
+    currentBlackSegments = {};
+    currentWhiteSegments = {};
+    /**
+     * Sets for each color containing the points found so far in the currently examined column
+     */
+    currentBlackPoints = {};
+    currentWhitePoints = {};
+
+    toRemove = {}; // placeholder set to prevent a ConcurrentModificationException
+    for(var x = 0; x < this.gameData.getBoardData().getSize(); x++) {
+        // Populates the current segments sets.
+        currentBlackSegments = {};
+        currentWhiteSegments = {};
+        currentBlackPoints = {};
+        currentWhitePoints = {};
+        for(var y = 0; y < this.gameData.getBoardData().getSize(); y++) {
+            // first add any new segments that use the new point to account for shared vertices
+            if (this.getCell(x, y).hasBlackPiece) {
+                for (var i in currentBlackPoints) {
+                    if (currentBlackPoints.hasOwnProperty(i)) {
+                        continue;
+                    }
+                    var obj = {top: currentBlackPoints[i], bottom: y};
+                    currentBlackSegments[obj] = obj; // does this add it like a set entry like I think it does?
+                }
+            }
+            if (this.getCell(x, y).hasWhitePiece) {
+                for (var i in currentWhitePoints) {
+                    if (currentWhitePoints.hasOwnProperty(i)) {
+                        continue;
+                    }
+                    var obj = {top: currentWhitePoints[i], bottom: y};
+                    currentWhiteSegments[obj] = obj; // does this add it like a set entry like I think it does?
+                }
+            }
+            // then remove previous points which are blocked by the new point of an opposing color
+            if (this.getCell(x, y).hasBlackPiece) {
+                currentWhitePoints = {};
+            }
+            if (this.getCell(x, y).hasWhitePiece) {
+                currentBlackPoints = {};
+            }
+            // finally, add the new points no matter what since they can't have been blocked yet
+            if (this.getCell(x, y).hasBlackPiece) {
+                currentBlackPoints[y] = y;
+            }
+            if (this.getCell(x, y).hasWhitePiece) {
+                currentWhitePoints[y] = y;
+            }
+        }
+
+        // Searches for parallel pairs in pair map and scores accordingly.
+        for(var p in currentBlackSegments) {
+            if (object.hasOwnProperty(p)) {
+                continue;
+            }
+            if(typeof blackSegmentColumns[p] !== 'undefined' && x - blackSegmentColumns[p].column > 1) { // quick fix until we have cap states
+                noMoreCaptures = false;
+                var obj = blackSegmentColumns[p];
+                for(var i = obj.column; i <= x; i++){
+                for(var j = obj.x; j <= obj.y; j++){
+                    this.place(i, j, State.BLACK);
+                }
+                }
+            }
+        }
+        for(var p in currentWhiteSegments) {
+            if (object.hasOwnProperty(p)) {
+                continue;
+            }
+            if(typeof whiteSegmentColumns[p] !== 'undefined' && x - whiteSegmentColumns[p].column > 1) { // quick fix until we have cap states
+                noMoreCaptures = false;
+                var obj = whiteSegmentColumns[p];
+                for(var i = obj.column; i <= x; i++){
+                for(var j = obj.x; j <= obj.y; j++){
+                    this.place(i, j, State.WHITE);
+                }
+                }
+            }
+        }
+
+        // Removes any pairs blocked by things in the current row.
+        for(var i in currentBlackPoints) {
+            if (currentBlackPoints.hasOwnProperty(i)) {
+                continue;
+            }
+            for(var p in currentWhiteSegments) {
+                if (currentWhiteSegments.hasOwnProperty(p)) {
+                    continue;
+                }
+                if(p.x == i || p.y == i) {
+                    toRemove[p] = p;
+                }
+            }
+        }
+        for(var p in toRemove) {
+            if(toRemove.hasOwnProperty(p)) {
+                continue;
+            }
+            delete whiteSegmentColumns[p];
+        }
+        toRemove = {};
+
+        for(var i in currentWhitePoints) {
+            if (currentWhitePoints.hasOwnProperty(i)) {
+                continue;
+            }
+            for(var p in currentBlackSegments) {
+                if (currentBlackSegments.hasOwnProperty(p)) {
+                    continue;
+                }
+                if(p.x == i || p.y == i) {
+                    toRemove[p] = p;
+                }
+            }
+        }
+        for(var p in toRemove) {
+            if(toRemove.hasOwnProperty(p)) {
+                continue;
+            }
+            delete blackSegmentColumns[p];
+        }
+        toRemove = {};
+
+        // Adds newly found segments to the columns maps.
+        for(var p in currentBlackSegments) {
+            if (currentBlackSegments.hasOwnProperty(p)) {
+                continue;
+            }
+            blackSegmentColumns[p] = {top: p.top, bottom: p.bottom, column: x};
+        }
+        for(var p in currentWhiteSegments) {
+            if (currentWhiteSegments.hasOwnProperty(p)) {
+                continue;
+            }
+            whiteSegmentColumns[p] = {top: p.top, bottom: p.bottom, column: x};
+        }
+    }
+    return noMoreCaptures;
 };
