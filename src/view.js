d9540c1c5a82a80a2abf4a5fc99e01149b15920e
Matthew Faw <matthewfaw@gmail.com>
2017-02-04 14:06:28 -0500

    adds temporary fix to require(...) syntax

--- src/view.js
+++ src/view.js
@@ -1,5 +1,6 @@
-var React = require('react')
-var ReactDOM = require('react-dom')
+//ADD THIS
+//var React = require('react')
+//var ReactDOM = require('react-dom')
 //TODO add type protection to the stateless functional components.
 
 /**
