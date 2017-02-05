c733967593b382e254ef1d4c863ff67141daeb8c
Matthew Faw <matthewfaw@gmail.com>
2017-02-05 00:55:25 -0500

    sets up a basic server that works in development

---
+++ server/routes.js
@@ -0,0 +1,12 @@
+import path from 'path';
+
+export const route = (app) => {
+    app.get('/', (req, res) => {
+        if (process.env.NODE_ENV === 'production') {
+            console.log('derp')
+            res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+        } else {
+            res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
+        }
+    })
+}
